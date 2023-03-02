2a499291a52f3abf4828a303ec41ea47ec50f2b6
sukhendra <mpct5jan1998@gmail.com>
2023-03-03 02:30:46 +0530

    pattern10

---
+++ pattern10.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+void pattern1(int n) {
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < i; j++) {
+      cout << "*";
+    }
+    cout << endl;
+  }
+}
+
+void pattern2(int n) {
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n - i * 1; j++) {
+      cout << "*";
+    }
+    cout << endl;
+  }
+}
+
+int main() {
+  int n;
+  cout << "Enter the number: ";
+  cin >> n;
+  pattern1(n);
+  pattern2(n);
+}
